5fc5066c2f5cec0e11d9c50ee4a56e962c1fd676
Missariella <macxdewit@gmail.com>
2021-06-13 21:46:54 +0700

    setup symfony 5 basic
    setup site - create entities

---
+++ src/Entity/Webcategorie.php
@@ -0,0 +1,239 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use App\Repository\WebcategorieRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * @ORM\Entity(repositoryClass=WebcategorieRepository::class)
+ */
+class Webcategorie
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(type="string")
+     * @Assert\NotBlank
+     */
+    private $title;
+
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(type="string")
+     * @Assert\NotBlank
+     */
+    private $color;
+
+
+    /**
+     * @var User
+     *
+     * @ORM\ManyToOne(targetEntity="App\Entity\User")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $author;
+
+    /**
+     * Indicate if the product is enabled (available in store).
+     *
+     * @var bool
+     * @ORM\Column(type="boolean")
+     */
+    private $enabled = false;
+
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="position", type="integer")
+     */
+    private $position;
+
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(type="string")
+     * @Assert\NotBlank
+     */
+    private $ftitle;
+
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(type="string")
+     * @Assert\NotBlank
+     */
+    private $furl;
+
+
+    /**
+     * @var Weblink[]|Collection
+     *
+     * @ORM\OneToMany(
+     *      targetEntity="Weblink",
+     *      mappedBy="webcategorie",
+     *      orphanRemoval=true,
+     *      cascade={"persist"}
+     * )
+     @ORM\OrderBy({"position" = "ASC"})
+     */
+    private $weblinks;
+
+
+    public function __construct()
+    {
+
+        $this->weblinks = new ArrayCollection();
+
+    }
+
+
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(?string $title): void
+    {
+        $this->title = $title;
+    }
+
+
+    public function getColor(): ?string
+    {
+        return $this->color;
+    }
+
+    public function setColor(?string $color): void
+    {
+        $this->color = $color;
+    }
+
+    public function getAuthor(): ?User
+    {
+        return $this->author;
+    }
+
+    public function setAuthor(User $author): void
+    {
+        $this->author = $author;
+    }
+
+
+    /**
+     * Set if the product is enable.
+     *
+     * @param bool $enabled
+     */
+    public function setEnabled($enabled)
+    {
+        $this->enabled = $enabled;
+    }
+
+    /**
+     * Is the product enabled?
+     *
+     * @return bool
+     */
+    public function getEnabled()
+    {
+        return $this->enabled;
+    }
+
+    /**
+     * Alias of getEnabled.
+     *
+     * @return bool
+     */
+    public function isEnabled()
+    {
+        return $this->getEnabled();
+    }
+
+
+
+    public function getFtitle(): ?string
+    {
+        return $this->ftitle;
+    }
+
+    public function setFtitle(?string $ftitle): void
+    {
+        $this->ftitle = $ftitle;
+    }
+
+
+
+
+    public function getFurl(): ?string
+    {
+        return $this->furl;
+    }
+
+    public function setFurl(?string $furl): void
+    {
+        $this->furl = $furl;
+    }
+
+
+
+    /**
+     * @return int
+     */
+    public function getPosition(): ?int
+    {
+        return $this->position;
+    }
+    /**
+     * @param int $position
+     */
+    public function setPosition(?int $position)
+    {
+        $this->position = $position;
+    }
+
+
+
+
+    public function getWeblinks(): Collection
+    {
+        return $this->weblinks;
+    }
+
+    public function addWeblink(Weblink $weblink): void
+    {
+        $weblink->setWebcategorie($this);
+        if (!$this->weblinks->contains($weblink)) {
+            $this->weblinks->add($weblink);
+        }
+    }
+
+    public function removeWeblink(Weblink $weblink): void
+    {
+        $this->weblinks->removeElement($weblink);
+    }
+
+
+}
---
+++ src/Entity/Weblinks.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\WeblinkRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * @ORM\Entity(repositoryClass=WeblinkRepository::class)
+ */
+class Weblink
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+
+    /**
+     * @var Webcategorie
+     *
+     * @ORM\ManyToOne(targetEntity="Webcategorie", inversedBy="weblinks")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $webcategorie;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(type="string")
+     * @Assert\NotBlank
+     */
+    private $title;
+
+
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(type="string")
+     * @Assert\NotBlank
+     */
+    private $url;
+
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private int $position = 0;
+
+
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(?string $title): void
+    {
+        $this->title = $title;
+    }
+
+
+    public function getUrl(): ?string
+    {
+        return $this->url;
+    }
+
+    public function setUrl(?string $url): void
+    {
+        $this->url = $url;
+    }
+
+
+    public function getWebcategorie(): ?Webcategorie
+    {
+        return $this->webcategorie;
+    }
+
+    public function setWebcategorie(Webcategorie $webcategorie): void
+    {
+        $this->webcategorie = $webcategorie;
+    }
+
+
+    public function getPosition(): int
+    {
+        return $this->position;
+    }
+
+    public function setPosition(int $position): void
+    {
+        $this->position = $position;
+    }
+
+
+
+}
