87a498b620728ddc20891837da8ea96d453b4be2
Missariella <macxdewit@gmail.com>
2021-06-14 15:53:26 +0700

    update easyadmin sidebar
    update easyadmin sidebar

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -7,6 +7,7 @@ use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Webcategorie;
 
 class DashboardController extends AbstractDashboardController
 {
@@ -21,12 +22,35 @@ class DashboardController extends AbstractDashboardController
     public function configureDashboard(): Dashboard
     {
         return Dashboard::new()
-            ->setTitle('Projectsites');
+            // the name visible to end users
+            ->setTitle('ACME Corp.')
+            // you can include HTML contents too (e.g. to link to an image)
+            ->setTitle('ACME <span class="text-small">Corp.</span>')
+
+      
+            ->renderSidebarMinimized(false)
+
+          ;
+
     }
 
     public function configureMenuItems(): iterable
     {
-        yield MenuItem::linktoDashboard('Dashboard', 'fa fa-home');
+         return [
+
+         yield MenuItem::linktoDashboard('Dashboard', 'fa fa-home'),
         // yield MenuItem::linkToCrud('The Label', 'fas fa-list', EntityClass::class);
+        // MenuItem::section('Blog'),
+        //MenuItem::subMenu('Blog', 'fa fa-article')->setSubItems([
+       // MenuItem::linkToCrud('Categories', 'fa fa-tags', Webcategorie::class),
+       // MenuItem::linkToCrud('Posts', 'fa fa-file-text', Webcategorie::class),
+       // MenuItem::linkToCrud('Comments', 'fa fa-comment', Webcategorie::class),
+        //]),
+
+
+
+
+
+                ];
     }
 }
